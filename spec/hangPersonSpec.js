db63f09003b7e91c6a3fb7f017f982ee01115d28
Aldric Giacomoni <trevoke@gmail.com>
2014-06-01 21:58:15 -0400

    I can guess a single letter

--- spec/hangPersonSpec.js
+++ spec/hangPersonSpec.js
@@ -1,15 +1,21 @@
 describe("hangPerson", function() {
     var testDictionary = ["banana", "ananas", "grapefruit"];
-    var hangMan = new hangPerson(testWords);
+    var hangMan = new hangPerson(testDictionary);
     it("should give me one of many words", function() {
-        var secretWord = hangMan.secretWord();
+        var secretWord = hangMan.secretWord;
         var words = hangMan.words;
-        expect(words).toContain(secretWord);
+        expect(testDictionary).toContain(secretWord);
     });
     it("should display all underscores when the word has not been guessed", function() {
         var displayedWord = hangMan.displayWord();
-        expect(displayedWord.length).toEqual(hangMan.secretWord().length);
+        expect(displayedWord.length).toEqual(hangMan.secretWord.length);
         var allUnderscores = displayedWord.split('').every(function(element) { return element == "_"; });
         expect(allUnderscores).toBe(true);
+    });
+    it("should display the guessed letter and all underscores", function() {
+        hangMan.guess('a');
+        var displayWord = hangMan.displayWord();
+        expect(displayWord.split('')).toContain('a');
+        expect(displayWord.split('')).toContain('_');
     });
 });
--- src/hangPerson.js
+++ src/hangPerson.js
@@ -1,12 +1,17 @@
 function hangPerson(dictionary) {
     this.dictionary = dictionary;
-    this.word = this.randomWord();
-    this.currentGuess = this.word.replace(/./g, '_');
+    this.secretWord = this.randomWord();
+    this.currentGuess = this.secretWord.replace(/./g, '_');
 }
 
 hangPerson.prototype = {
-    secretWord: function secretWord() {
-        return this.word;
+    guess: function guess(letter) {
+        // TODO: ensure it's just a letter and not more of the word
+        if(this.secretWord.indexOf(letter) == -1) {
+            return false;
+        }
+        var re = new RegExp('[^' + letter + ']', 'g');
+        this.currentGuess = this.secretWord.replace(re, '_');
     },
     randomWord: function randomWord() {
         return this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
